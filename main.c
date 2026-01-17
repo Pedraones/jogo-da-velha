3a7b1953308bb286aeeccd9d0bdcc49378462cb8
Pedraones <perobr2018@gmail.com>
2026-01-17 11:21:54 -0300

    Primeiro if com C

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void main(){
+    int num = 90;
+    if(num == 10){
+        printf("Variavel num e igual a 10");
+    }
+    else if(num > 10){
+        printf("Variavel num e maior do que 10");
+    }
+    else{
+        printf("Variavel num e menor do que 10");
+    }
+}
