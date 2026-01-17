2ecdd19486566d1ad2e6a571851f66c7ca2d587f
Pedraones <perobr2018@gmail.com>
2026-01-17 12:49:26 -0300

    Switch case

--- main.c
+++ main.c
@@ -2,7 +2,24 @@
 #include <string.h>
 
 void main(){
-    int num1 = 2;
-    int num2 = num1 == 2 ? num1 : 4;
-    printf("%d", num2);
+    int num1;
+    char text[90];
+
+    printf("Digite um numero \n");
+    scanf("%d", &num1);
+
+    switch (num1)
+    {
+    case 1:
+        printf("num1 = 1 \n");
+        break;
+    
+    case 2:
+        printf("num1 = 2 \n");
+        break;
+
+    default:
+        printf("num1 != 2 \n");
+        break;
+    }
 }
