d75b6a3a2646cb17feca45042914dd71c1efae82
Pedraones <perobr2018@gmail.com>
2026-01-17 12:09:23 -0300

    Entendendo enum

--- main.c
+++ main.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int countStatic(void){
-    static int num = 0;
-    return num++;
-}
-
-int countVariable(void){
-    int num = 0;
-    return num++;
-}
-
 void main(){
-    printf("Funcao com variavel static: ");
-    for(int i = 0; i <= 5; i++) printf("%d", countStatic());
-    printf("\n");
-    printf("Funcao com variavel nao static: ");
-    for(int i = 0; i <= 5; i++) printf("%d", countVariable());
+    enum{
+        a, b, c, d
+    };
+    printf("%d", d);
 }
