e0da84c4d0e537b2975cb111341e590681d7321e
Pedraones <perobr2018@gmail.com>
2026-01-17 12:00:45 -0300

    Entendendo variavel static

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+int countStatic(void){
+    static int num = 0;
+    return num++;
+}
+
+int countVariable(void){
+    int num = 0;
+    return num++;
+}
+
 void main(){
-    for(int i = 0; i <= 5; i++){
-        printf(i);
-    }
+    printf("Funcao com variavel static: ");
+    for(int i = 0; i <= 5; i++) printf("%d", countStatic());
+    printf("\n");
+    printf("Funcao com variavel nao static: ");
+    for(int i = 0; i <= 5; i++) printf("%d", countVariable());
 }
