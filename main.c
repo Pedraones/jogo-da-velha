48a2860ea72606da4a7b999bcfe50c179cf8d8f1
Pedraones <perobr2018@gmail.com>
2026-01-17 17:20:38 -0300

    diretiva #include para importar macros de arquivos criados particularmente (operations.c)

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
-#define teste(n) n+n
+#include "operations.c"
 
 int main(){
-
-    printf("%d ", teste(1));
+    printf("%d \n", soma(2,4));
+    printf("%d \n", subtrai(20,4));
+    printf("%d \n", multiplica(12,4));
+    printf("%d \n", dividi(2,4));
 }
---
+++ operations.c
@@ -0,0 +1,4 @@
+#define soma(a, b) a+b
+#define subtrai(a, b) a-b
+#define multiplica(a, b) a*b
+#define dividi(a, b) a/b
