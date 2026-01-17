f56c152114abeff5bd46d6fc522c7b6630bffdee
Pedraones <perobr2018@gmail.com>
2026-01-17 11:28:00 -0300

    Primeiro for com C

--- main.c
+++ main.c
@@ -2,14 +2,7 @@
 #include <string.h>
 
 void main(){
-    int num = 90;
-    if(num == 10){
-        printf("Variavel num e igual a 10");
-    }
-    else if(num > 10){
-        printf("Variavel num e maior do que 10");
-    }
-    else{
-        printf("Variavel num e menor do que 10");
+    for(int i = 0; i <= 5; i++){
+        printf(i);
     }
 }
