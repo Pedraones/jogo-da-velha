0bfc1d6f6507a0d6a94bb9659cbed4d0c582bc60
Pedraones <perobr2018@gmail.com>
2026-01-17 16:21:39 -0300

    Primeiro while

--- main.c
+++ main.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
 void main(){
-    int num1;
-    char text[90];
-
-    printf("Digite um numero \n");
-    scanf("%d", &num1);
-
-    switch (num1)
-    {
-    case 1:
-        printf("num1 = 1 \n");
-        break;
-    
-    case 2:
-        printf("num1 = 2 \n");
-        break;
-
-    default:
-        printf("num1 != 2 \n");
-        break;
+    int num = 0;
+    while(num <= 4){
+        printf("num = %d\n", num);
+        num++;
     }
 }
