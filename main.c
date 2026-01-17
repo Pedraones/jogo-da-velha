3ff1ce994d08b1756b961fc3b92ac0f7e798cec3
Pedraones <perobr2018@gmail.com>
2026-01-17 12:27:21 -0300

    Operadores logicos

--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 #include <string.h>
 
 void main(){
-    enum{
-        a, b, c, d
-    };
-    printf("%d", d);
+    int num1 = 2;
+    int num2 = 0;
+
+    //!variavel = se variavel igual a false
+    if(!num1) printf("num1 = 0 \n");
+    else printf("num1 > 0 \n");
+
+    //&& as 2 condicoes devem ser verdadeiras para retornar true
+    if(!num1 && !num2) printf("as 2 variaveis sao falsas, = 0 \n");
+    else printf("1 ou nenhuma variavel sao nulas \n");
+
+    //|| uma das 2 condicoes devem ser verdadeiras para retornar true
+    if(!num1 || !num2) printf("alguma ou as 2 variaveis sao falsas, = 0");
+    else printf("nenhuma das variaveis sao falsas, = 0");
 }
