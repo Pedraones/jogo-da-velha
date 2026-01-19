15fb0456a8a9c3a00ba3b0b09196ac40d8aa2a28
Pedraones <perobr2018@gmail.com>
2026-01-19 10:49:57 -0300

    Entendendo recursividade

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "operations.c"
 
+//n1 quem e multiplicado
+//n2 quem multiplica
+int multi(unsigned int n1, int n2) {
+    if( n2==1 ) return n1;
+    return n1 + multi(n1, n2-1);
+} 
+
 int main(){
-    printf("%d \n", soma(2,4));
-    printf("%d \n", subtrai(20,4));
-    printf("%d \n", multiplica(12,4));
-    printf("%d \n", dividi(2,4));
+    printf("%d",multi(3, 2));
 }
