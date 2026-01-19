df1487c80c07ea5e5a64e4c9b0073caeca236de6
Pedraones <perobr2018@gmail.com>
2026-01-19 11:03:40 -0300

    Vizualizacao de cada chamada da funcao recursiva

--- main.c
+++ main.c
@@ -3,11 +3,16 @@
 
 //n1 quem e multiplicado
 //n2 quem multiplica
-int multi(unsigned int n1, int n2) {
+void chamada(int n1, int n2){
+    printf("multi(%d, %d) \n", n1, n2);
+}
+
+int multi(unsigned int n1, unsigned int n2) {
+    chamada(n1, n2);
     if( n2==1 ) return n1;
     return n1 + multi(n1, n2-1);
 } 
 
 int main(){
-    printf("%d",multi(3, 2));
+    printf("resultado: %d",multi(3, 2));
 }
