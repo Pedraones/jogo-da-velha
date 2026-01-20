635d3b561487cdc9e3444f74b8a346d32a719604
Pedraones <perobr2018@gmail.com>
2026-01-20 11:18:52 -0300

    Visualizando o resultado de cada chamada da funcao recursiva

--- main.c
+++ main.c
@@ -7,12 +7,23 @@ void chamada(int n1, int n2){
     printf("multi(%d, %d) \n", n1, n2);
 }
 
+void devolve(int r){
+    printf("%d \n", r);
+}
+
 int multi(unsigned int n1, unsigned int n2) {
+    int r;
     chamada(n1, n2);
-    if( n2==1 ) return n1;
-    return n1 + multi(n1, n2-1);
+    if( n2== 0 ) {
+        return 0;
+    }
+    else {
+        r = n1 + multi(n1, n2-1);
+        devolve(r);
+    }
+    return r;
 } 
 
 int main(){
-    printf("resultado: %d",multi(3, 2));
+    printf("resultado: %d",multi(5, 4));
 }
