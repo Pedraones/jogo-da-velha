0369bb1a969787c1c0701b83b2dd54e35f96b254
Pedraones <perobr2018@gmail.com>
2026-01-28 17:14:33 -0300

    Algoritmo: busca binaria em ordem decrescente

--- main.c
+++ main.c
@@ -7,17 +7,16 @@ void BuscaBinaria(int x, int L[], int width){
     int IInicial = 0;
     int IUltimo = width-1;
     
-    
     while(IInicial <= IUltimo){
         int Meio = (IInicial + IUltimo) / 2;
         if(x == L[Meio]) return printf("X foi encontrado \n");
-        if(x < L[Meio]) IUltimo = Meio-1;
-        else IInicial = Meio + 1;
+        if(x < L[Meio]) IInicial = Meio + 1;
+        else IUltimo = Meio-1;
     }
     return printf("X nao foi encontrado \n");
 }
 
 int main(void) {
-    int L[] = {10, 16, 27, 31, 33, 37, 41, 49, 53, 57, 68, 69, 72, 77, 84, 89, 95, 99};
-    BuscaBinaria(99, L, 18);
+    int L[] = {99, 95, 89, 84, 77, 72, 69, 68, 57, 53, 49, 41, 37, 33, 31, 27, 16, 10};
+    BuscaBinaria(53, L, 18);
 } 
