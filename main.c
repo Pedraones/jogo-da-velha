551f3145283d1871a03993ab130c7da0e7cea66e
Pedraones <perobr2018@gmail.com>
2026-01-30 19:35:49 -0300

    Struct com membros com quantidade de bits de armazenamento limitados

--- main.c
+++ main.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 
 typedef struct {
-    int Dia;
-    int Mes;
-    int Ano
-} Data;
-
-typedef struct {
-    char Nome[30];
-    Data Nascimento;
-} Pessoa;
+    int a : 1;
+    int b : 6;
+} StorageBits;
 
 int main(void) {
-    Pessoa Pessoa1 = {"Pedro"};
-    Pessoa1.Nascimento.Ano = 2007;
-    Pessoa1.Nascimento.Mes = 11;
-    Pessoa1.Nascimento.Dia = 29;
-
-    printf("Nome: %s, Nascimento: %d/%d/%d", Pessoa1.Nome, Pessoa1.Nascimento.Dia, Pessoa1.Nascimento.Mes, Pessoa1.Nascimento.Ano);
+    StorageBits Bits = {12, 9};
+    printf("%d, %d \n", Bits.a, Bits.b);
 } 
