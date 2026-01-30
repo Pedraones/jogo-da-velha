8c9918d443ca0e4dcf816f112b69e93ef8851a0d
Pedraones <perobr2018@gmail.com>
2026-01-30 18:22:14 -0300

    Structs aninhada

--- main.c
+++ main.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-typedef struct data{
+typedef struct {
     int Dia;
     int Mes;
-    int Ano;
-};
+    int Ano
+} Data;
+
+typedef struct {
+    char Nome[30];
+    Data Nascimento;
+} Pessoa;
 
 int main(void) {
-    struct data natal;
-    natal.Ano = 2026;
-    natal.Mes = 12;
-    natal.Dia = 25;
-    printf("%d \n",natal.Dia);
+    Pessoa Pessoa1 = {"Pedro"};
+    Pessoa1.Nascimento.Ano = 2007;
+    Pessoa1.Nascimento.Mes = 11;
+    Pessoa1.Nascimento.Dia = 29;
+
+    printf("Nome: %s, Nascimento: %d/%d/%d", Pessoa1.Nome, Pessoa1.Nascimento.Dia, Pessoa1.Nascimento.Mes, Pessoa1.Nascimento.Ano);
 } 
