d07ea8450f531e2c952f7a7797c4657c380d384f
Pedraones <perobr2018@gmail.com>
2026-01-30 08:55:36 -0300

    Algoritmo de ordenacao por insercao

--- main.c
+++ main.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 
-int selmin(int v[], int i, int n){
-    int k = i;
-    int j;
-    for(j = i+1; j < n; j++){
-        if(v[k] > v[j]){
-            k = j;
+void insercao(int v[], int n) {
+    int i, x, j, y;
+
+    for(i=1; i<n; i++) {
+        x = v[i]; 
+        j = i;
+
+        while(j > 0 && v[j-1] > x){
+            y = v[j-1];
+            v[j-1] = x;
+            v[j] = y;
+            j--;
         }
-    }
-    return k;
-}
-
-void trocas(int v[], int n) {
-    int i, k;
-
-    for(i=0; i<n-1; i++){
-        k = selmin(v, i, n);
-        
-        int x = v[i];
-        v[i] = v[k];
-        v[k] = x;
     }
 
     for(i = 0; i < n; i++) printf("%d \n", v[i]);
 } 
 
 int main(void) {
-    int L[] = {33, 68, 49, 16, 95, 37, 10, 57, 27, 84, 72, 31, 89, 41, 53, 69, 99, 77};
-    trocas(L, 18);
+    int L[] = { 68, 49, 95, 16, 90, 100, 03, 327 };
+    insercao(L, 8);
 } 
