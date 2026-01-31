c70d3ab59e2237569b0eeeba8e5e988d4ec825d9
Pedraones <perobr2018@gmail.com>
2026-01-31 10:07:01 -0300

    Ponteiros

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-typedef struct {
-    int a : 1;
-    int b : 6;
-} StorageBits;
-
 int main(void) {
-    StorageBits Bits = {12, 9};
-    printf("%d, %d \n", Bits.a, Bits.b);
+    int *pontero;
+    int num = 5;
+    pontero = &num;
+    printf("%p \n", pontero);
 } 
