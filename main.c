94038d9816d73e61be9f1c33e95d34ff710b7438
Pedraones <perobr2018@gmail.com>
2026-02-10 15:50:57 -0300

    Alocacao dinamica do tamanho de vetores

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    int *pontero;
-    int num = 5;
-    pontero = &num;
-    printf("%p \n", pontero);
-} 
+int main() {
+    int *vetor, tamanho, i;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tamanho);
+
+    vetor = malloc(tamanho*sizeof(int));
+
+    if(vetor == NULL) exit(1);
+
+    for(i = 0; i < tamanho; i++){
+        printf("Digite um valor: ");
+        scanf("%d", &vetor[i]);
+    }
+
+    for(i = 0; i < tamanho; i++){
+        printf("Valor do elemento %d: %d \n", i, vetor[i]);
+    }
+}
