8ba60102e23b6fcabbc07a3018f0fcd17204073c
Pedraones <perobr2018@gmail.com>
2026-02-14 11:35:35 -0300

    Implementacao de um no na Iesima posicao

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 typedef struct link{
     char item;
     struct link *prox;
 } *Link;
+
+Link acessa(Link p, int i) {
+    while( i > 0 && p!=NULL) {
+        p = p->prox;
+        i--;
+    }
+    return p;
+} 
 
 void insere(Link *inicio, char letra){
     Link aloca = malloc(sizeof(Link));
@@ -15,24 +22,17 @@ void insere(Link *inicio, char letra){
     
 }
 
-Link acessa(Link p, int i) {
-    while( i > 0 && p!=NULL) {
-        p = p->prox;
-        i--;
-    }
-    return p;
-} 
-
 int main(void) {
-    Link inicio;    
-    Link teste;
+    Link inicio;
 
     insere(&inicio, 'b');
     insere(&inicio, 'c');
+    
+    Link p = acessa(inicio,1);
+    insere(&p->prox, '*'); 
+    Link q = acessa(inicio,1);
 
-    teste = acessa(inicio, 1);
-
-    printf("%c \n", teste->item);
+    printf("%c \n", q->item);
 
     return 0;
 } 
