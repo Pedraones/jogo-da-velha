721ff1cfc871de8535f3ec8a49a2e4e5f48ecb60
Pedraones <perobr2018@gmail.com>
2026-02-14 10:57:52 -0300

    Implementacao de novo elemento no inicio de uma lista encadeada

--- main.c
+++ main.c
@@ -6,7 +6,22 @@ typedef struct link{
     struct link *prox;
 } *Link;
 
+void insere(Link *inicio, char letra){
+    Link aloca = malloc(sizeof(Link));
+    
+    aloca->item = letra;
+    aloca->prox = *inicio;
+    *inicio = aloca;
+    
+}
+
 int main(void) {
+    Link inicio;    
+
+    insere(&inicio, 'b');
+    insere(&inicio, 'c');
+
+    printf("%p \n", inicio->prox);
 
     return 0;
 } 
