a1645e7972d8c2d7e003bd70b2aa7d5aa25ec199
Pedraones <perobr2018@gmail.com>
2026-02-14 10:57:13 -0300

    Estrutura de uma lista encadeada

--- main.c
+++ main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int *vetor, tamanho, i;
+typedef struct link{
+    char item;
+    struct link *prox;
+} *Link;
 
-    printf("Digite o tamanho do vetor: ");
-    scanf("%d", &tamanho);
+int main(void) {
 
-    vetor = malloc(tamanho*sizeof(int));
-
-    if(vetor == NULL) exit(1);
-
-    free(vetor);
-}
+    return 0;
+} 
