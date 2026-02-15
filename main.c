edce7cb7e9dacfd6d733e50edc6808d213aa3d4e
Pedraones <perobr2018@gmail.com>
2026-02-15 10:30:39 -0300

    Modelo falho de remocao do iesimo numero de uma lista encadeada

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 typedef struct link{
     char item;
     struct link *prox;
-} *Link;
+} Link;
 
-Link acessa(Link p, int i) {
+Link *acessa(Link *p, int i) {
     while( i > 0 && p!=NULL) {
         p = p->prox;
         i--;
@@ -13,26 +13,42 @@ Link acessa(Link p, int i) {
     return p;
 } 
 
-void insere(Link *inicio, char letra){
-    Link aloca = malloc(sizeof(Link));
+void insere(Link **inicio, char letra){
+    Link *aloca = (Link *)malloc(sizeof(Link));
     
-    aloca->item = letra;
+    aloca->item = letra; 
     aloca->prox = *inicio;
-    *inicio = aloca;
+    *inicio = aloca; 
+}
+
+void remover(Link **elemento){
+    Link *copia;
+    copia = *elemento; //*ser_exluido e o elemento que ser remover
+    Link *ser_excluido;
+    ser_excluido = &*copia;
+    *copia = *ser_excluido->prox;
     
+    printf("Na funcao remover: %c \n", ser_excluido->item);
+    free(ser_excluido);
 }
 
 int main(void) {
-    Link inicio;
+    Link *inicio;
 
     insere(&inicio, 'b');
     insere(&inicio, 'c');
+    insere(&inicio, 'a');
+
+    Link *q;
+    q = acessa(inicio,2);
+    printf("Antes da funcao remover: %c \n", q->item);
     
-    Link p = acessa(inicio,1);
-    insere(&p->prox, '*'); 
-    Link q = acessa(inicio,1);
+    //ordem: 0=a, 1 = c, 2 = b
 
-    printf("%c \n", q->item);
-
+    remover(&q);
+    
+    q = acessa(inicio,2);
+    
+    printf("Depois da funcao remover: %c \n", q->item);
     return 0;
 } 
