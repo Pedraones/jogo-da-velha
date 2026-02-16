6f8dd2493611217def94054f6db1695f853e6410
Pedraones <perobr2018@gmail.com>
2026-02-16 10:00:48 -0300

    dev: feat - Funcionalidade de escolher qual jogador inicia implementada

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int escolha(){
+    int vez;
+
+    printf("Jogador 1: X, jogador 2: O \nQual jogador ira comecar jogando ? (Indique o numero do jogador)\n");
+    scanf("%d", &vez);
+
+    if(vez != 1 && vez != 2) {
+        system("clear");
+        printf("Digite um jogador valido\n");
+        vez = escolha();
+    };
+    
+    return vez;
+}
+
 int main(){
     char tabuleiro[9] = {'1', '2', '3','4', '5', '6','7', '8', '9'};
-    int i;
+    int i, vez;
+    
+    vez = escolha();
 
-    printf("%c | %c | %c\n%c | %c | %c\n%c | %c | %c\n", tabuleiro[0], tabuleiro[1], tabuleiro[2], tabuleiro[3], tabuleiro[4], tabuleiro[5], tabuleiro[6], tabuleiro[7], tabuleiro[8]);
+    //printf("%c | %c | %c\n%c | %c | %c\n%c | %c | %c\n", tabuleiro[0], tabuleiro[1], tabuleiro[2], tabuleiro[3], tabuleiro[4], tabuleiro[5], tabuleiro[6], tabuleiro[7], tabuleiro[8]);
+    //system("clear");
     
     return 0;
 }
