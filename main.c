14df38c97bef57e7360f2a074cb7545fa8627976
Pedraones <perobr2018@gmail.com>
2026-02-17 11:32:12 -0300

    dev: feat - Implementado a passagem de rodadas com a insercao do X ou O no lugar escolhido

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int escolha(){
+int escolha_quem_comeca(){
     int vez;
 
     printf("Jogador 1: X, jogador 2: O \nQual jogador ira comecar jogando ? (Indique o numero do jogador)\n");
@@ -10,7 +10,7 @@ int escolha(){
     if(vez != 1 && vez != 2) {
         system("clear");
         printf("Digite um jogador valido\n");
-        vez = escolha();
+        vez = escolha_quem_comeca();
     };
     
     return vez;
@@ -18,12 +18,21 @@ int escolha(){
 
 int main(){
     char tabuleiro[9] = {'1', '2', '3','4', '5', '6','7', '8', '9'};
-    int i, vez;
+    int vez, posicao, rodada;
     
-    vez = escolha();
+    vez = escolha_quem_comeca();
 
-    //printf("%c | %c | %c\n%c | %c | %c\n%c | %c | %c\n", tabuleiro[0], tabuleiro[1], tabuleiro[2], tabuleiro[3], tabuleiro[4], tabuleiro[5], tabuleiro[6], tabuleiro[7], tabuleiro[8]);
-    //system("clear");
+    for(rodada = 1; rodada<=9;){
+        printf("%c | %c | %c\n%c | %c | %c\n%c | %c | %c\n", tabuleiro[0], tabuleiro[1], tabuleiro[2], tabuleiro[3], tabuleiro[4], tabuleiro[5], tabuleiro[6], tabuleiro[7], tabuleiro[8]);
+
+        printf("Insira qual quadrado deseja inserir o O ou X: ");
+        scanf("%d", &posicao);
+        
+        if(vez == 1) tabuleiro[posicao-1] = 'X';
+        if(vez == 2) tabuleiro[posicao-1] = 'O';
+        rodada++;
+        system("clear");
+    }
     
     return 0;
 }
